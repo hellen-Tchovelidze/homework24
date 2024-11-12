90987d53d7b47013b135ddf4ad889c066773819c
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-11-12 22:12:01 +0400

    js

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ let hours = prompt("saatebi");
 let minutes = prompt("seiyvane wutebi");
 let seconds = prompt("seiyvanet wamebi");
 
-if (days) {
-  document.getElementById("DAYS").textContent = days;
-  document.getElementById("HOURS").textContent = hours;
-  document.getElementById("MINUTES").textContent = minutes;
-  document.getElementById("SECONDS").textContent = seconds;
-}
+
 
 if (isNaN(days) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
   alert("გთხოვთ, შეიყვანეთ მხოლოდ რიცხვები.");
 }
+else{
+    if (days) {
+        document.getElementById("DAYS").textContent = days;
+        document.getElementById("HOURS").textContent = hours;
+        document.getElementById("MINUTES").textContent = minutes;
+        document.getElementById("SECONDS").textContent = seconds;
+      }
+}
 days = hours = minutes = seconds = 0;
 
+
+
 let TimerCount = setInterval(() => {
-  seconds++;
-  if (seconds === 59) {
-    clearInterval(TimerCount);
-  }
-  console.log(TimerCount);
+    seconds++;
+    if (seconds === 59) {
+        clearInterval(TimerCount);
+    }
+    // console.log(TimerCount);
 }, 1000);
