e05ef586bf9ff24d607fbaa52c3b88fb965f9afd
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-11-14 18:06:26 +0400

    css js

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-let days = prompt(" შეიყვანეთ დღეები");
-let hours = prompt("saatebi");
-let minutes = prompt("seiyvane wutebi");
-let seconds = prompt("seiyvanet wamebi");
-
-
+let days = prompt("შეიყვანეთ დღეები");
+let hours = prompt("შეიყვანე საათი ");
+let minutes = prompt("შეიყვანე წუთები");
+let seconds = prompt("შეიყვანე წამები");
 
 if (isNaN(days) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
   alert("გთხოვთ, შეიყვანეთ მხოლოდ რიცხვები.");
 }
-else{
-    if (days) {
-        document.getElementById("DAYS").textContent = days;
-        document.getElementById("HOURS").textContent = hours;
-        document.getElementById("MINUTES").textContent = minutes;
-        document.getElementById("SECONDS").textContent = seconds;
-      }
+ else {
+  days = parseInt(days);
+  hours = parseInt(hours);
+  minutes = parseInt(minutes);
+  seconds = parseInt(seconds);
+
+  document.getElementById("DAYS").textContent = days;
+  days < 10 ? "0" + days : days;
+  document.getElementById("HOURS").textContent = hours;
+  hours < 10 ? "0" + hours : hours;
+  document.getElementById("MINUTES").textContent = minutes;
+  minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("SECONDS").textContent = seconds;
+  seconds < 10 ? "0" + seconds : seconds;
 }
-days = hours = minutes = seconds = 0;
 
-
-
-let TimerCount = setInterval(() => {
-    seconds++;
-    if (seconds === 59) {
-        clearInterval(TimerCount);
-    }
-    // console.log(TimerCount);
+const countdown = setInterval(() => {
+  if (seconds > 0) {
+    seconds--;
+  } else if (minutes > 0) {
+    minutes--;
+    seconds = 59;
+  } else if (hours > 0) {
+    hours--;
+    minutes = 59;
+    seconds = 59;
+  } else if (days > 0) {
+    days--;
+    hours = 23;
+    minutes = 59;
+    seconds = 59;
+  }
+  document.getElementById("DAYS").textContent = days < 10 ? "0" + days : days;
+  document.getElementById("HOURS").textContent =
+    hours < 10 ? "0" + hours : hours;
+  document.getElementById("MINUTES").textContent =
+    minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById("SECONDS").textContent =
+    seconds < 10 ? "0" + seconds : seconds;
 }, 1000);
